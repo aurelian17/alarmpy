d21649ac52b4f985962675e6f6d3f0b653cc759d
Aurelian Zanoschi <aurelian17@gmail.com>
2015-04-01 18:52:20 +0000

    Added first implementation for password class

---
+++ password.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+from time import sleep
+from sys import exit
+import keypad as alarm_kp
+
+## Initialize the keypad class.
+kp = alarm_kp.keypad()
+
+
+# Setup variables
+attempt = "00000"
+passcode = "1590E"
+counter = 0
+
+# Loop while waiting for a keypress
+while True:
+	# Loop to get a pressed digit
+	digit = None
+	while digit == None:
+		digit = kp.getKey()
+ 
+	# Print the result
+	print "Digit Entered:       %s"%digit
+	attempt = (attempt[1:] + str(digit))  
+	print "Attempt value:       %s"%attempt
+	
+	# Check for passcode match
+	if (attempt == passcode):
+		print "Your code was correct, goodbye."
+		exit()
+	else:
+		counter += 1
+		print "Entered digit count: %s"%counter
+		if (counter >= 5):
+			print "Incorrect code!"
+			sleep(3)
+			print "Try Again" 
+			sleep(1)
+			counter = 0
+	
+	sleep(0.5)

a75336eb42dceabb7b3d4db266afba96242128fa
Aurelian Zanoschi <aurelian17@gmail.com>
2015-04-03 17:31:47 +0300

    Updated the alarm configuration based on alarm_input class members

--- alarm_configuration.py
+++ alarm_configuration.py
@@ -9,7 +9,7 @@ v 1.0 First version containing only configuration for alarm inputs
 
 --------------------------------------------------------------------------------
 """
-
+import RPi.GPIO as GPIO
 import ConfigParser					#for configuration files
 import logging						#for logging
 import sys						#for logging
@@ -32,8 +32,7 @@ class alarm_configuration():
 	def initDebugging(self):
 		"initialization of the debugging system"
 		logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-		logging.basicConfig(format='%(asctime)s %(message)s')
-		logging.basicConfig(datefmt='%m/%d/%Y %I:%M:%S %p')
+		logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 		return
 
 	def writeInputsConfigFile(self, filename):
@@ -44,12 +43,12 @@ class alarm_configuration():
 			configuration = ConfigParser.ConfigParser()
 			#configuration.read(filename)
 			configuration.add_section('INPUTS')
-			configuration.set('INPUTS', 'NUMBER OF INPUTS', '8')
-			configuration.set('INPUTS', 'TYPE OF INPUTS', 'PULL_UP')
+			configuration.set('INPUTS', 'NUMBER', '8')
 			configuration.add_section('INPUT_0')
 			configuration.set('INPUT_0', 'GPIO_Number', 0)
+			configuration.set('INPUT_0', 'GPIO_Pull', GPIO.PUD_UP)
+			configuration.set('INPUT_0', 'GPIO_Edge', GPIO.FALLING)
 			configuration.set('INPUT_0', 'GPIO_Timeout', 100)
-			configuration.set('INPUT_0', 'GPIO_Edge', 'FALLING')
 			configuration.write(cfgfile)
 			cfgfile.close()
 

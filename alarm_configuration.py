1480eafa79575a973b87e3b89c07703d3442d2b1
Aurelian Zanoschi <aurelian17@gmail.com>
2015-04-07 15:39:37 +0300

    Moved implementation for reading class constants from configuration file to alarm_password class

--- alarm_configuration.py
+++ alarm_configuration.py
@@ -7,6 +7,7 @@ alarm_configuration.py
 Revision History
 v 1.0 First version containing only configuration for alarm inputs
 v 1.1 Added configuration for alarm passwords
+v 1.2 Moved tha password configuration in the alarm_password class
 
 --------------------------------------------------------------------------------
 """
@@ -21,7 +22,6 @@ class alarm_configuration():
 	# alarm_configuration class constants
 	alarm_inputs_filename = "alarm_inputs.cfg"
 	alarm_output_filename = "alarm_output.cfg"
-	alarm_passwords_filename = "alarm_password.cfg"
 	alarm_zones_filename = "alarm_zones.cfg"
 
 	def __init__(self):
@@ -80,64 +80,12 @@ class alarm_configuration():
 		logging.debug("-==alarm_configuration: readInputsConfigFile() exit")
 		return
 
-
-	def writePasswordsConfigFile(self, filename):
-		"writing the configuration file for alarm passwords with ConfigParser"
-		logging.debug("-==alarm_configuration: writePasswordsConfigFile(%s)==-", filename)
-		cfgfile = open(filename,'wb')
-		try:
-			configuration = SafeConfigParser()
-			configuration.add_section('PASSWORDS')
-			configuration.set('PASSWORDS', 'USERS', '3')
-			configuration.set('PASSWORDS', 'LENGTH', '4')
-			configuration.set('PASSWORDS', 'ACCEPT', 'E')
-			configuration.set('PASSWORDS', 'CLEAR', 'C')
-			configuration.add_section('PASSWORD_0')
-			configuration.set('PASSWORD_0', 'USERNAME', 'User0')
-			configuration.set('PASSWORD_0', 'PASSWORD', '0000')
-			configuration.add_section('PASSWORD_1')
-			configuration.set('PASSWORD_1', 'USERNAME', 'User1')
-			configuration.set('PASSWORD_1', 'PASSWORD', '1111')
-			configuration.add_section('PASSWORD_2')
-			configuration.set('PASSWORD_2', 'USERNAME', 'User2')
-			configuration.set('PASSWORD_2', 'PASSWORD', '2222')
-			configuration.write(cfgfile)
-			cfgfile.close()
-
-		except ConfigParser.ParsingError, err:
-			logging.error("-==alarm_configuration: writePasswordsConfigFile(): Cannot read %s:%s==-", filename, err)
-
-		logging.debug("-==alarm_configuration: writePasswordsConfigFile() exit==-")
-		return
-
-	def readPasswordsConfigFile(self, filename):
-		"reading the configuration file for alarm passwords with ConfigParser"
-		logging.debug("-==alarm_configuration: readPasswordsConfigFile(%s)==-", filename)
-		try:
-			configuration = SafeConfigParser()
-			configuration.read(filename)
-			print "--------------------------------------------------------------------"
-			print configuration
-			print "--------------------------------------------------------------------"
-			for section in configuration.sections():
-				print 'Section: ', section
-				print 'Options: ', configuration.options(section)
-				for name, value in configuration.items(section):
-					print '%s = %s' % (name, value)
-				print
-		except(ConfigObjError, IOError), e:
-			logging.error("-==alarm_configuration: readPasswordsConfigFile(): Cannot read %s : %s==-" ,filename, e)
-		logging.debug("-==alarm_configuration: readPasswordsConfigFile() exit")
-		return
-
 def main():
 	"main function"
 	logging.debug("-==alarm_configuration: main()==-"
 	ac = alarm_configuration()
 	ac.writeInputsConfigFile(ac.alarm_inputs_filename)
 	ac.readInputsConfigFile(ac.alarm_inputs_filename)
-	ac.writePasswordsConfigFile(ac.alarm_passwords_filename)
-	ac.readPasswordsConfigFile(ac.alarm_passwords_filename)
 	return
 
 if __name__ == "__main__":
--- alarm_password.py
+++ alarm_password.py
@@ -9,6 +9,8 @@ v 1.0 First version containing only import of keypad and calls for
 	getting key pressed and validating simple password
 v 1.1 Added alarm_password class and modified a little the 
 	implementation
+v 1.2 Added implementation for reading class constants from configuration
+	file
 
 --------------------------------------------------------------------------
 """
@@ -20,13 +22,36 @@ import alarm_keypad as alarm_kp
 class alarm_password():
 
 	# alarm_password class constants
-	attempt = "00000"
-	passcode = "1590E"
-	counter = 0
+	alarm_passwords_configuration_file = "alarm_password.cfg"
+	#attempt = "00000"
+	#passcode = "1590E"
+	#counter = 0
 
 	def __init__(self):
+		# Define alarm_password class variables
+		num_users = 0
+		passwd_len = 0
+		accept_char = ''
+		clear_char = ''
+		users = []
+		passwds = []
+
 		# Initialize the keypad class
 		kp = alarm_kp.alarm_keypad()
+
+		# Read the class properties from the configuration file
+		self.writePasswordsConfigFile(self.alarm_passwords_configuration_file)
+		self.readPasswordsConfigFile(self.alarm_passwords_configuration_file)
+
+		# Chech the class variables values:
+		print "number of users: %s"%self.num_users
+		print "passwords length: %s"%self.passwd_len
+		print "accept character: %s"%self.accept_char
+		print "clear character: %s"%self.clear_char
+		print users
+		print passwds
+		attempt = zfill(self.passwd_len + 1)
+		print "attempt: %s"%self.attempt
 
 		# Loop while waiting for a keypress
 		while True:
@@ -56,11 +81,70 @@ class alarm_password():
 
 			sleep(0.5)
 
+	def writePasswordsConfigFile(self, filename):
+		"writing the configuration file for alarm passwords with ConfigParser"
+		logging.debug("-==alarm_configuration: writePasswordsConfigFile(%s)==-", filename)
+		cfgfile = open(filename,'wb')
+		try:
+			configuration = SafeConfigParser()
+			configuration.add_section('PASSWORDS')
+			configuration.set('PASSWORDS', 'USERS', '3')
+			configuration.set('PASSWORDS', 'LENGTH', '4')
+			configuration.set('PASSWORDS', 'ACCEPT', 'E')
+			configuration.set('PASSWORDS', 'CLEAR', 'C')
+			configuration.add_section('PASSWORD_0')
+			configuration.set('PASSWORD_0', 'USERNAME', 'User0')
+			configuration.set('PASSWORD_0', 'PASSWORD', '0000')
+			configuration.add_section('PASSWORD_1')
+			configuration.set('PASSWORD_1', 'USERNAME', 'User1')
+			configuration.set('PASSWORD_1', 'PASSWORD', '1111')
+			configuration.add_section('PASSWORD_2')
+			configuration.set('PASSWORD_2', 'USERNAME', 'User2')
+			configuration.set('PASSWORD_2', 'PASSWORD', '2222')
+			configuration.write(cfgfile)
+			cfgfile.close()
+
+		except ConfigParser.ParsingError, err:
+			logging.error("-==alarm_configuration: writePasswordsConfigFile(): Cannot read %s:%s==-", filename, err)
+
+		logging.debug("-==alarm_configuration: writePasswordsConfigFile() exit==-")
+		return
+
+	def readPasswordsConfigFile(self, filename):
+		"reading the configuration file for alarm passwords with ConfigParser"
+		logging.debug("-==alarm_configuration: readPasswordsConfigFile(%s)==-", filename)
+		try:
+			configuration = SafeConfigParser()
+			configuration.read(filename)
+			print "--------------------------------------------------------------------"
+			print configuration
+			print "--------------------------------------------------------------------"
+			for section in configuration.sections():
+				print 'Section: ', section
+				print 'Options: ', configuration.options(section)
+				for name, value in configuration.items(section):
+					print '%s = %s' % (name, value)
+				print
+
+			self.num_users = configuration.get('PASSWORDS', 'USERS')
+			self.passwd_len = configuration.get('PASSWORDS', 'LENGTH')
+			self.accept_char = configuration.get('PASSWORDS', 'ACCEPT')
+			self.clear_char = configuration.get('PASSWORDS', 'CLEAR')
+			for i in range (0, self.num_users):
+				user.insert(i, configuration.get('PASSWORD_' + str(i), 'USERNAME'))
+				passwds.insert(i, configuration.get('PASSWORD_' + str(i), 'PASSWORD'))
+
+		except(ConfigObjError, IOError), e:
+			logging.error("-==alarm_configuration: readPasswordsConfigFile(): Cannot read %s : %s==-" ,filename, e)
+		logging.debug("-==alarm_configuration: readPasswordsConfigFile() exit")
+		return
+
 def main():
 	"main function"
 	print "-==alarm_password main()==-"
 	# Initialize the passwords class
 	passwd = alarm_password()
+
 
 if __name__ == '__main__':
 	main()

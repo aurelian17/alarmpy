2afe1000f774496622c36c6fd1e19dbaceb38050
Aurelian Zanoschi <aurelian17@gmail.com>
2015-09-18 08:47:02 +0300

    Added script for bluetooth dongle support

---
+++ bluetooth_support.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This script is used to install all the dependencies for bluetooth unlock support for alarmpi
+
+sudo apt-get install bluez-utils python-gtk2 python-glade2 python-configobj python-bluez blueproximity
+
